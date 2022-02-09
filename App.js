227875c41932af9e0a740197bf39550ce8c66f37
shashankgautam-12 <60915373+shashankgautam-12@users.noreply.github.com>
2022-02-09 11:07:30 +0530

    code push

--- App.js
+++ App.js
@@ -1,92 +1,104 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Modal, View, Text, ActivityIndicator} from 'react-native';
+import codePush from 'react-native-code-push';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+let codePushOptions = {checkFrequency: codePush.CheckFrequency.MANUAL};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [progress, setProgress] = useState(false);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  useEffect(() => {
+    codePush.sync(
+      {
+        updateDialog: true,
+        installMode: codePush.InstallMode.IMMEDIATE,
+      },
+      codePushStatusDidChange,
+      codePushDownloadDidProgress,
+    );
+  }, []);
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  function codePushStatusDidChange(syncStatus) {
+    switch (syncStatus) {
+      case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+        console.log('Checking for update.');
+        break;
+      case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+        console.log('Download packaging....');
+        break;
+      case codePush.SyncStatus.AWAITING_USER_ACTION:
+        console.log('Awaiting user action....');
+        break;
+      case codePush.SyncStatus.INSTALLING_UPDATE:
+        console.log('Installing update');
+        setProgress(false);
+        break;
+      case codePush.SyncStatus.UP_TO_DATE:
+        console.log('codepush status up to date');
+        break;
+      case codePush.SyncStatus.UPDATE_IGNORED:
+        console.log('update cancel by user');
+        setProgress(false);
+        break;
+      case codePush.SyncStatus.UPDATE_INSTALLED:
+        console.log('Update installed and will be applied on restart.');
+        setProgress(false);
+        break;
+      case codePush.SyncStatus.UNKNOWN_ERROR:
+        console.log('An unknown error occurred');
+        setProgress(false);
+        break;
+    }
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  function codePushDownloadDidProgress(progress) {
+    setProgress(progress);
+  }
+
+  console.log('progress value++', progress);
+
+  const showProgressView = () => {
+    return (
+      <Modal visible={true} transparent>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0,0.8)',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              backgroundColor: 'white',
+              borderRadius: 8,
+              padding: 16,
+            }}>
+            <Text>In Progress.......</Text>
+
+            <View style={{alignItems: 'center'}}>
+              <Text style={{marginTop: 16}}>{`${(
+                Number(progress?.receivedBytes) / 1048576
+              ).toFixed(2)}MB/${(
+                Number(progress?.totalBytes) / 1048576
+              ).toFixed(2)}`}</Text>
+              <ActivityIndicator style={{marginVertical: 8}} color={'blue'} />
+              <Text>
+                {(
+                  (Number(progress?.receivedBytes) /
+                    Number(progress?.totalBytes)) *
+                  100
+                ).toFixed(0)}
+                %
+              </Text>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{flex: 1}}>{!!progress ? showProgressView() : null}</View>
   );
 };
 
@@ -109,4 +121,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default codePush(codePushOptions)(App);
